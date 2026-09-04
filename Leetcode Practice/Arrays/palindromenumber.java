2b970ebf39e8dec8e8f8513d9c0575ed6839e4f8
dillirajan46 <rajandilli@gmail.com>
2026-09-04 20:06:27 +0530

    palindrome problem solved

---
+++ Leetcode Practice/Arrays/palindromenumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp = x;
+        int ans = 0;
+        while(x!=0){
+            int lastd = x % 10;
+            ans = ans * 10 + lastd;
+            x = x / 10;
+        }
+        if(ans==temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
